5745280a749a418ef8127123aa61a996a61d7b31
Jon Arne Westgaard <jon@wstgrd.com>
2011-09-23 15:39:11 +0200

    Started on File-menu

--- src/prosjekt1.java
+++ src/prosjekt1.java
@@ -10,9 +10,42 @@ public class prosjekt1 extends JFrame {
 		setLayout( layout);
 		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.insets = new Insets (2,2,2,2);
-	        JLabel label1;
-		label1 = new JLabel("Her trenger vi litt mer innhold...");
-		add(label1);
+	    
+		
+		// Filemenu
+		JMenuBar bar = new JMenuBar();
+		setJMenuBar(bar);
+		JMenu fileMenu = new JMenu ("File");
+		fileMenu.setMnemonic('F');
+		JMenuItem newItem = new JMenuItem("New");
+		newItem.setMnemonic('N');
+		fileMenu.add(newItem);
+		JMenuItem loadItem = new JMenuItem("Load");
+		loadItem.setMnemonic('L');
+		fileMenu.add(loadItem);
+		JMenuItem saveItem = new JMenuItem("Save");
+		saveItem.setMnemonic('S');
+		fileMenu.add(saveItem);
+		JMenuItem saveasItem = new JMenuItem("Save as");
+		saveasItem.setMnemonic('a');
+		fileMenu.add(saveasItem);
+		JMenuItem previewItem = new JMenuItem("Preview");
+		saveasItem.setMnemonic('P');
+		fileMenu.add(previewItem);
+		JMenuItem generateItem = new JMenuItem("Generate Java code");
+		generateItem.setMnemonic('G');
+		fileMenu.add(generateItem);
+		JMenuItem exitItem = new JMenuItem("Exit");
+		exitItem.setMnemonic('E');
+		fileMenu.add(exitItem);
+		JMenu editMenu = new JMenu ("Edit");
+		editMenu.setMnemonic('E');
+		JMenu helpMenu = new JMenu ("Help");
+		helpMenu.setMnemonic('H');
+		bar.add(fileMenu);
+		bar.add(editMenu);
+		bar.add(helpMenu);
+		
 		
 		pack();
 		setVisible (true);
