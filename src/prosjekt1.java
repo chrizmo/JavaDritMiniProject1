42811cbaea33f5d32ff3124ec38153d2621fec2a
Jon Arne Westgaard <jon@wstgrd.com>
2011-09-23 14:58:35 +0200

    GUI v0.1

---
+++ src/prosjekt1.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+public class prosjekt1 extends JFrame {
+	
+	public prosjekt1 () {
+		super ("prosjekt1");
+		GridBagLayout layout = new GridBagLayout();
+		setLayout( layout);
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.insets = new Insets (2,2,2,2);
+	    JLabel label1;
+		label1 = new JLabel("Her trenger vi litt mer innhold...");
+		add(label1);
+		
+		
+		JLabel label2;
+		label2 = new JLabel("File");
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		add(label2);
+		
+		pack();
+		setVisible (true);
+		
+	}
+	
+	public static void main (String args[]) {
+	prosjekt1 p1 = new prosjekt1();
+	p1.setSize(770, 300);
+	p1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		
+	}
+	
+}
